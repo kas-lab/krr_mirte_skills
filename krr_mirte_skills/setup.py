33eb46d22284e8d129ccb69785764ba707b69e04
Elvin <45923444+EGAlberts@users.noreply.github.com>
2025-02-26 15:39:30 +0100

    initial packages

---
+++ krr_mirte_skills/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'krr_mirte_skills'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ega',
+    maintainer_email='45923444+EGAlberts@users.noreply.github.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = krr_mirte_skills.my_node:main'
+        ],
+    },
+)
